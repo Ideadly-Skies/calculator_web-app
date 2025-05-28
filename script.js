cde6aaa97ad74453c9f523c0df4fba1c4715a523
IdeadlySkies <Obie.kal22@gmail.com>
2025-05-28 22:15:08 +0700

    feat: display number and operator on screen

--- script.js
+++ script.js
@@ -39,3 +39,47 @@ keys.addEventListener('click', e => {
     
     }
 })
+
+const display = document.querySelector('.calculator__display')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        
+        if (!action) {
+            if (displayedNum === '0') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
+
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.'
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            let operatorSymbol = '';
+            if (action === 'add') operatorSymbol = '+';
+            if (action === 'subtract') operatorSymbol = '-';
+            if (action === 'multiply') operatorSymbol = '×';
+            if (action === 'divide') operatorSymbol = '÷';
+
+            display.textContent = displayedNum + operatorSymbol;
+
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator'
+        }
+
+        // Remove .is-depressed class from all keys
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+    }
+})
